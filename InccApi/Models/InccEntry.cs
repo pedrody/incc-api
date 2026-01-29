b61fa149aefcbf972c7a98a20d93644588750b4c
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-01-29 18:35:49 -0300

    feat: add InccEntry

---
+++ InccApi/Models/InccEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InccApi.Models;
+
+[Table("incc_entries")]
+public class InccEntry
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Column(TypeName = "date")]
+    public DateTime ReferenceDate { get; set; }
+    
+    [Required]
+    [Range(0.0001, double.MaxValue)]
+    [Column(TypeName = "decimal(18,4)")]
+    public decimal Value { get; set; }
+}
