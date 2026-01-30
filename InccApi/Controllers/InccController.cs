4b39d7cb125569b3d909d709b2f6235285481085
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-01-30 15:30:23 -0300

    feat: add endpoint to get all INCC entries

---
+++ InccApi/Controllers/InccController.cs
@@ -0,0 +1,31 @@
+﻿using InccApi.Models;
+using InccApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InccApi.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class InccController : ControllerBase
+{
+    private readonly IInccRepository _inccRepository;
+
+    public InccController(IInccRepository inccRepository)
+    {
+        _inccRepository = inccRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<InccEntry>>> Get()
+    {
+        var inccEntries = await _inccRepository.GetAllAsync();
+
+        if (inccEntries is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(inccEntries);
+    }
+}
