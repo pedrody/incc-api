eec1f0e0d5c7997197dc36676c18cb4c7e7299b9
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-03-28 21:18:23 -0300

    feat: add InccCreateDto

---
+++ InccApi/DTOs/InccCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InccApi.DTOs;
+
+public class InccCreateDto
+{
+    public DateTime ReferenceDate { get; set; }
+    public decimal Value { get; set; }
+    public double MonthlyVariation { get; set; }
+}
